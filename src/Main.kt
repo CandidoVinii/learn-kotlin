b8a76b67f77696c8ed6783ce4a4d24db1ad581c9
Vinicius Aquino <vinicius.dias2020@engenharia.ufjf.br>
2024-02-04 19:36:29 -0300

    feat: creat new function for the three exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,11 @@
 fun main() {
-    // val calc = salaryCalc(2000.00)
-    // print(calc)
-    val stepCalc = stepsCalc(0.50, 10.00)
-    print(stepCalc)
+//     val calc = salaryCalc(2000.00)
+//      print(calc)
+//    val stepCalc = stepsCalc(0.50, 10.00)
+//    print(stepCalc)
+    val showTheLargeast = showTheLargest(39 , 20)
+    print(showTheLargeast)
 }
-
 
 // calculate the 25% increase in the salary received as a parameter
 fun salaryCalc(salary: Double): Double {
@@ -15,7 +16,12 @@ fun salaryCalc(salary: Double): Double {
 
 // calculate the many steps will hava to climb height
 fun stepsCalc(heightStep: Double, heightReach: Double): Double {
-    val result = heightStep / heighStep
+    val result = heightStep / heightReach
     return result
 }
+// take two numbers and show the largest
+fun showTheLargest(a: Int, b: Int): Int{
+    return if (a <= b) b else a
+}
+
 
