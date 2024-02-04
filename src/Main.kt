491675a3b4cca2fd49bd4e10f228ec1f8baec5df
Vinicius Aquino <vinicius.dias2020@engenharia.ufjf.br>
2024-02-04 19:30:08 -0300

    feat: create function for second exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,21 @@
 fun main() {
-    val calc = salaryCalc(2000.00)
-    print(calc)
+    // val calc = salaryCalc(2000.00)
+    // print(calc)
+    val stepCalc = stepsCalc(0.50, 10.00)
+    print(stepCalc)
 }
 
+
+// calculate the 25% increase in the salary received as a parameter
 fun salaryCalc(salary: Double): Double {
     val calcIncreaseSalary = (salary * 25) / 100
     val calcFinallySalary = salary + calcIncreaseSalary
     return calcFinallySalary
 }
+
+// calculate the many steps will hava to climb height
+fun stepsCalc(heightStep: Double, heightReach: Double): Double {
+    val result = heightStep / heighStep
+    return result
+}
+
